9847f5299baddf78ca4bcf06f09bd8b06900ecda
kaan özdemir <kaan@inworks.de>
2023-04-14 15:27:31 +0200

    AUth Page and sign in form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './routs/home/home.component';
 import Navigation from './routs/navigation/navigation.component';
-import SignIn from './routs/sign-in/sign-in.component';
+import Authentication from './routs/authentication/authentication.component';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Home />} />
-        <Route path="signIn" element={<SignIn />} />
+        <Route path="auth" element={<Authentication />} />
       </Route>
     </Routes>
   );
---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import {
+  createAuthUserWithEmailAndPassword,
+  createUserDocumentFromAuth,
+  signInAuthUserWithEmailAndPassword,
+  signInWithGooglePopup
+} from '../../utils/firebase/firebase.utils';
+import FormInput from '../form-input/form-input.component';
+import './sign-in-form.styles.scss';
+import Button from '../button/button.component';
+const defaultFormFields = {
+  email: '',
+  password: ''
+};
+
+const SignInForm = () => {
+  const [formFields, setFormFields] = useState(defaultFormFields);
+  const { email, password } = formFields;
+
+  const resetFormFields = () => {
+    setFormFields(defaultFormFields);
+  };
+
+  const signInWithGoogle = async () => {
+    const { user } = await signInWithGooglePopup();
+    await createUserDocumentFromAuth(user);
+  };
+  const handelSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await signInAuthUserWithEmailAndPassword(email, password);
+      console.log('RESPONSE SIGNIN', response);
+      resetFormFields();
+    } catch (e) {
+      switch (e.code) {
+        case 'auth/wrong-password':
+          alert('Falsches Passwort');
+          break;
+        case 'auth/user-not-found':
+          alert('User existiert nicht');
+          break;
+        default:
+          console.log(e);
+      }
+    }
+  };
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+
+    setFormFields({ ...formFields, [name]: value });
+  };
+  return (
+    <div className="sign-up-container">
+      <h2>Du hast schon ein Account ?</h2>
+      <span> Melde dich mit deiner E-Mail und deinem Passwort an!</span>
+      <form onSubmit={handelSubmit}>
+        <FormInput
+          label="E-Mail"
+          type="email"
+          required
+          onChange={handleChange}
+          name="email"
+          value={email}
+        />
+
+        <FormInput
+          label="Password"
+          type="password"
+          required
+          onChange={handleChange}
+          name="password"
+          value={password}
+        />
+        <div className="buttons-container">
+          <Button type="submit">Anmelden</Button>
+          <Button type="button" buttonType="google" onClick={signInWithGoogle}>
+            Google Login
+          </Button>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default SignInForm;
